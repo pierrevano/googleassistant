f866174ea9b0bb21fa3c227d80a0e876dc5ce2e1
Pierre Vanoverschelde <pierre.vano@gmail.com>
2020-03-15 18:48:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// Define required packages
+const express = require("express")
+const bodyParser = require("body-parser")
+const qs = require("qs")
+const axios = require("axios")
+const {
+    actionssdk,
+    SimpleResponse
+} = require("actions-on-google")
+
+// Create app instance
+const app = actionssdk()
+
+// Define MAIN intent to start a conversation
+app.intent("actions.intent.MAIN", conv => {
+    conv.ask("<speak>Bienvenue sur XXX</speak>");
+});
+
+// Define TEXT intent to get user inputs
+app.intent("actions.intent.TEXT", (conv, input) => {
+    // Define body data
+    const data = {
+        "XXX": input,
+        "YYY": "YYY"
+    };
+
+    // Send POST request
+    const options = {
+        method: "POST",
+        headers: {
+            "accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "charset": "utf-8"
+        },
+        data: qs.stringify(data),
+        url: endpoint
+    };
+
+    return axios(options)
+        .then(function(response) {
+            // Get text key from response
+            var b64string = response.data.values.text;
+            // Decode response and remove html tags
+            var answer = (Buffer.from(b64string, "base64")).toString("utf8").replace(/<[^>]*>/g, "");
+            return answer;
+        })
+        .then(function(answer) {
+            // Send answer to Google Assistant
+            conv.ask(answer);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+});
+
+// Define NO_INPUT intent when the Assistant doesn't receive any input from the user
+app.intent("actions.intent.NO_INPUT", (conv) => {
+    const repromptCount = parseInt(conv.arguments.get("REPROMPT_COUNT"));
+    if (repromptCount === 0) {
+        conv.ask("Que voulez-vous dire ?");
+    } else if (repromptCount === 1) {
+        conv.ask("Désolé je n'ai toujours pas compris, pouvez-vous répéter ?");
+    } else if (conv.arguments.get("IS_FINAL_REPROMPT")) {
+        conv.close("OK, réessayons plus tard.");
+    }
+});
+
+// Define CANCEL intent to end a conversation
+app.intent("actions.intent.CANCEL", (conv) => {
+    conv.close("OK, à bientôt !");
+});
+
+// Start Express for Heroku deployment
+const expressApp = express().use(bodyParser.json())
+expressApp.post("/fulfillment", app)
+expressApp.listen(process.env.PORT || 5000)
